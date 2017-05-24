4c759553f1ba221f77259b0b4c04b23b87f46775
douglasdrdc <douglasdrdc@gmail.com>
2017-05-23 21:27:05 -0300

    Formatação da data para formato de fuso brasilia

---
+++ EmDica.Web/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using EmDica.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmDica.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        public static readonly log4net.ILog loggerAplicacao = log4net.LogManager.GetLogger("LogAplicacao");
+    }
+}
---
+++ EmDica.Web/Helpers/Util.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmDica.Web.Helpers
+{
+    public class Util
+    {
+        public static string ConverteDataHorarioBrasilia(DateTime data)
+        {
+            string dataRetorno = string.Empty;
+
+            DateTime timeUtc = DateTime.UtcNow;
+            TimeZoneInfo kstZone = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time");
+            DateTime dateTimeBrasilia = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, kstZone);
+
+            dataRetorno = string.Format("{0}-{1}-{2} {3}:{4}:{5}",
+                dateTimeBrasilia.Year,
+                dateTimeBrasilia.Month.ToString().PadLeft(2,'0'),
+                dateTimeBrasilia.Day.ToString().PadLeft(2, '0'),
+                dateTimeBrasilia.Hour.ToString().PadLeft(2, '0'),
+                dateTimeBrasilia.Minute.ToString().PadLeft(2, '0'),
+                dateTimeBrasilia.Second.ToString().PadLeft(2, '0')
+                );
+
+            return dataRetorno;
+        }
+    }
+}
